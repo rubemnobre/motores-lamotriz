da6b2495fb93c78663eb3bf7264164b1ba540426
rubemnobre <rubemjrx@hotmail.com>
2021-08-11 13:34:40 -0300

    Revert "b"
    This reverts commit 41138e3fe3a0f997cd64b596b919daf5faa7042c.

--- main.c
+++ main.c
@@ -556,19 +556,9 @@ void ConfigureDAC(){
 }
 
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
 int cont_controle = 0;
 unsigned int tc = 0;
 __interrupt void timer0_isr(){
-=======
-unsigned long i_amostra = 0;
-float pos;
-int cont_controle = 0;
-unsigned int tc = 0;
-
-__interrupt void timer0_isr(){
-    PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
->>>>>>> Stashed changes
 =======
 unsigned long i_amostra = 0;
 float pos;
@@ -669,10 +659,7 @@ __interrupt void timer0_isr(){
         I_atual_q = I_q;
 
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
 =======
-=======
->>>>>>> Stashed changes
         data1->id = I_d;
         data1->iq = I_q;
 
@@ -767,12 +754,9 @@ __interrupt void timer0_isr(){
             double ki = 6.6334e-05;
 
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
             //double kp = 0.0002;
             //double ki = 1.99e-4;
             float v_controle = refmras;
-=======
->>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
             erro_Velo = ref_Velo - v_controle;
@@ -791,10 +775,7 @@ __interrupt void timer0_isr(){
             }
         }
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
         //ref_kq = iq;
-=======
->>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 
@@ -807,14 +788,11 @@ __interrupt void timer0_isr(){
         erro_cq =  ref_kq - I_q;
 
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
         //LEI DE CONTROLE:
         //Controladores eixo d e q.
         v_atual_d =103*erro_cd -100.4 * erro_cd_ant1  + v_d_ant ;
         v_atual_q =103* erro_cq -100.4* erro_cq_ant1 + v_q_ant ;
 =======
-=======
->>>>>>> Stashed changes
         float kpi = 103, kii = 100.4;
         v_atual_d = v_d_ant + kpi*erro_cd - kii*erro_cd_ant1;
         v_atual_q = v_q_ant + kpi*erro_cq - kii*erro_cq_ant1;
@@ -882,14 +860,9 @@ __interrupt void timer0_isr(){
 
     saida = 1;
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
     CpuTimer0Regs.TCR.bit.TIF = 1;
     PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
-=======
-    i_amostra++;
-    data1->j = i_amostra;
->>>>>>> Stashed changes
 =======
     i_amostra++;
     data1->j = i_amostra;
@@ -898,10 +871,6 @@ __interrupt void timer0_isr(){
 
 __interrupt void adca1_isr(void){
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-    DINT;
->>>>>>> Stashed changes
 =======
     DINT;
 >>>>>>> Stashed changes
@@ -935,11 +904,8 @@ __interrupt void adca1_isr(void){
     DacaRegs.DACVALS.all = Velo_ADC;
     DacbRegs.DACVALS.all = refmras * 2.048;
 
-<<<<<<< Updated upstream
     GpioDataRegs.GPADAT.bit.GPIO15 = 0;
 =======
-=======
->>>>>>> Stashed changes
     refobs = ref_OBS(vc_obs, vb_obs, va_obs, ia, ib, ic);
 
     DacaRegs.DACVALS.all = Velo_avg * 2.048;
@@ -957,9 +923,6 @@ __interrupt void adca1_isr(void){
     AdcaRegs.ADCINTFLGCLR.bit.ADCINT1 = 1; //clear INT1 flag
     PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
     EINT;
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
 >>>>>>> Stashed changes
 }
 
